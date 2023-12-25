85911ccc7d5892de9566f369d5898a86f6bab37c
Vincent Siao <vincent@asana.com>
2023-12-24 23:30:58 -0800

    Add react-router-dom

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: null,
+    children: [
+      {
+        path: "/rooms/:roomId",
+        element: null,
+      },
+    ],
+  },
+]);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
