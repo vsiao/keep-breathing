8c516f910498181ba1b6d54c07d6c03bfc3d4160
Vincent Siao <vincent@asana.com>
2023-12-25 22:54:02 -0800

    Add prettier and format repo

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import Home from './home/Home';
-import GameRoom from './game/GameRoom';
-import { store } from './store/store';
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import Home from "./home/Home";
+import GameRoom from "./game/GameRoom";
+import { store } from "./store/store";
 
 const router = createBrowserRouter([
   {
@@ -28,14 +28,14 @@ const router = createBrowserRouter([
 ]);
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement,
 );
 root.render(
   <React.StrictMode>
     <Provider store={store}>
       <RouterProvider router={router} />
     </Provider>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
